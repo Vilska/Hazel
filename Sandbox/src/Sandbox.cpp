b7fbb98e05ad461b4f0e9784d5021938ee1f0ec6
Vilska <106695564+Vilska@users.noreply.github.com>
2022-09-04 19:00:26 +0300

    GLFW update, ImGui setup.

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,23 +1,24 @@
 #include <Hazel.h>
 
-class ExampleLayer : public Hazel::Layer {
-public:
-	ExampleLayer()
-		: Layer("Example") {}
-
-	void OnUpdate() override {
-		HZ_INFO("UPDATE");
-	}
-
-	void OnEvent(Hazel::Event& event) override {
-		HZ_TRACE("{0}", event);
-	}
-};
+//class ExampleLayer : public Hazel::Layer {
+//public:
+//	ExampleLayer()
+//		: Layer("Example") {}
+//
+//	void OnUpdate() override {
+//		HZ_INFO("UPDATE");
+//	}
+//
+//	void OnEvent(Hazel::Event& event) override {
+//		HZ_TRACE("{0}", event);
+//	}
+//};
 
 class Sandbox : public Hazel::Application {
 public:
 	Sandbox() {
-		PushLayer(new ExampleLayer());
+		//PushLayer(new ExampleLayer());
+		PushOverlay(new Hazel::ImGuiLayer());
 	}
 
 	~Sandbox() {
