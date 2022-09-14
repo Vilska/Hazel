f1b7d20b37f5b6e73d1c2079abc45f9653288444
Vilska <106695564+Vilska@users.noreply.github.com>
2022-09-14 23:00:58 +0300

    2D Renderer setup.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -10,29 +10,7 @@ Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f) {}
 
 void Sandbox2D::OnAttach() {
-	m_SquareVertexArray = Hazel::VertexArray::Create();
 
-	float squareVertices[5 * 4] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
-	};
-
-	Hazel::Ref<Hazel::VertexBuffer> squareVertexBuffer;
-	squareVertexBuffer.reset(Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-	squareVertexBuffer->SetLayout({
-		{ Hazel::ShaderDataType::Float3, "a_Position" }
-		});
-	m_SquareVertexArray->AddVertexBuffer(squareVertexBuffer);
-
-	uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-
-	Hazel::Ref<Hazel::IndexBuffer> squareIndexBuffer;
-	squareIndexBuffer.reset(Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
-	m_SquareVertexArray->SetIndexBuffer(squareIndexBuffer);
-
-	m_SquareShader = Hazel::Shader::Create("assets/shaders/FlatColor.glsl");
 }
 
 void Sandbox2D::OnDetach() {
@@ -46,14 +24,14 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts) {
 	Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 	Hazel::RenderCommand::Clear();
 
-	Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
+	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_SquareShader)->Bind();
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_SquareShader)->UploadUniformFloat4("u_Color", m_SquareColor);
+	//std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_SquareShader)->Bind();
+	//std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_SquareShader)->UploadUniformFloat4("u_Color", m_SquareColor);
+	//Hazel::Renderer::Submit(m_SquareShader, m_SquareVertexArray, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
-	Hazel::Renderer::Submit(m_SquareShader, m_SquareVertexArray, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
-
-	Hazel::Renderer::EndScene();
+	Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Hazel::Renderer2D::EndScene();
 }
 
 void Sandbox2D::OnImGuiRender() {
